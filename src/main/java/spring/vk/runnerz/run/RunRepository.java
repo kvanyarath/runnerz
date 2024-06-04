5d96dcfe42dc39091f4f55381edae5b93cd5eb05
KET Van Yarath <kvanyarath@gmail.com>
2024-06-04 21:49:03 +0700

    CRUD with postgres

--- src/main/java/spring/vk/runnerz/run/RunRepository.java
+++ src/main/java/spring/vk/runnerz/run/RunRepository.java
@@ -1,58 +1,10 @@
 package spring.vk.runnerz.run;
 
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.ListCrudRepository;
 
-import jakarta.annotation.PostConstruct;
 
-@Repository
-public class RunRepository {
-    private List<Run> runs = new ArrayList<>();
-
-    List<Run> findAll() {
-        return runs;
-    }
-
-    Optional<Run> findById(Integer id) {
-        return runs.stream()
-                    .filter(run -> run.id() == id)
-                    .findFirst();
-    }
-
-    void create(Run run) {
-        runs.add(run);
-    }
-
-    void update(Run run, Integer id) {
-        Optional<Run> runToUpdate = findById(id);
-        if (runToUpdate.isPresent()) {
-            runs.set(runs.indexOf(runToUpdate.get()), run);
-        }
-    }
-
-    void delete(Integer id) {
-        runs.removeIf(run -> run.id().equals(id));
-    }
-
-    @PostConstruct
-    private void init() {
-        runs.add(new Run(1,
-            "First Run",
-            LocalDateTime.now(), 
-            LocalDateTime.now().plus(30, ChronoUnit.MINUTES),
-            3,
-            Location.OUTDOOR));
-        
-            runs.add(new Run(2,
-            "Secondd Run",
-            LocalDateTime.now(), 
-            LocalDateTime.now().plus(60, ChronoUnit.MINUTES),
-            5,
-            Location.INDOOR));
-    }
+public interface RunRepository extends ListCrudRepository<Run, Integer> {
+    List<Run> findByLocation(String location);
 }
---
+++ src/main/java/spring/vk/runnerz/run/Runs.java
@@ -0,0 +1,7 @@
+package spring.vk.runnerz.run;
+
+import java.util.List;
+
+public record Runs(List<Run> runs) {
+
+}
